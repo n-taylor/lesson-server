3a867c786826045dda1b096f9e66f1f5708ef238
n-taylor <natekeithtaylor@gmail.com>
2018-09-01 14:27:54 -0600

    Use body parser

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 
 const port = 3000; // nginx routes port 80 request to port 3000
 
+app.use(bodyParser.json());
+
 // Route requests using the routes.js file
 require('./routes')(app)
-
-
 
 app.listen(port, () => { console.log('Listening on port ' + port) })
