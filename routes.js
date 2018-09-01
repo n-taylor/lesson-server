41951ac3b165ce1b02c4fc16c0c5d4c455a2851c
n-taylor <natekeithtaylor@gmail.com>
2018-09-01 09:41:53 -0600

    Change login route to get user by name

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
     app.get('/', main.home);
     app.get('/connect', db.connectTest);
 
-    app.get('/user/login/:username', db.getUser);
+    app.get('/user/byName/:username', db.getUser);
     app.get('/user/byOrg/:org', db.getUsersByOrg);
     app.post('/user/update', db.updateUser);
 
