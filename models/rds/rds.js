931d90240e82994e5f5b9550918f2431142d7207
n-taylor <natekeithtaylor@gmail.com>
2018-08-31 23:22:57 -0600

    Add single quotes around appropriate fields in db queries

--- models/rds/rds.js
+++ models/rds/rds.js
@@ -1,6 +1,6 @@
 const pool = require('./pool');
 
-const query_user_by_name = "SELECT * FROM users WHERE user_name = ";
+const query_user_by_name = "SELECT * FROM users WHERE user_name = '";
 
 /**
  * Gets the user by name from the database.
@@ -9,7 +9,7 @@ const query_user_by_name = "SELECT * FROM users WHERE user_name = ";
  */
 exports.getUserByName = function(username, callback){
     pool.getConnection(function(error, connection){
-        connection.query(query_user_by_name + username, function(error, rows){
+        connection.query(query_user_by_name + username + "'", function(error, rows){
             if (error || !rows[0]){
                 callback(error);
             }
